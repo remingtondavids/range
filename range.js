ed0620b9886508d202c6ac20f27de9aa3d00ac22
remingtondavids <53348044+remingtondavids@users.noreply.github.com>
2022-04-15 02:02:06 -0400

    saving to repo

---
+++ range.js
@@ -0,0 +1,20 @@
+function range(start, end, step=1) {
+    return {
+        [Symbol.iterator]() {
+            return this;
+        },
+        next() {
+            if (start < end) {
+                start += step;
+                return {value: start, done: false};
+            }
+            return {done: true, value: end};
+
+        }
+    }
+}
+
+
+for (const n of range(0, 20, 5)) {
+    console.log(n);
+}
